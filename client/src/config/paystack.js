9058c1ad243a7082258f699b0b57045f7d0eb498
King <kingeshiebor@gmail.com>
2023-10-17 13:32:23 +0100

    Added paystack config

---
+++ client/src/config/paystack.js
@@ -0,0 +1,13 @@
+const PaystackConfig = (email, cost) => {
+    const config = {
+      reference: new Date().getTime().toString(),
+      email: email,
+      //Currency in kobo
+      amount: (cost* 100),
+      publicKey: import.meta.env.VITE_PAYSTACK_API,
+    };
+  
+    return config;
+  };
+  
+  export default PaystackConfig;
