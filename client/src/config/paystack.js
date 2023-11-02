5b48176d053389c19143de266a0de8c246691625
King <kingeshiebor@gmail.com>
2023-11-02 20:08:20 +0100

    Added price in kobo for charges

--- client/src/config/paystack.js
+++ client/src/config/paystack.js
@@ -3,7 +3,7 @@ const PaystackConfig = (email, cost) => {
       reference: new Date().getTime().toString(),
       email: email,
       //Currency in kobo
-      amount: (cost* 100) + 100 +(0.015*cost),
+      amount: (cost* 100) + (100*100) +(0.015*cost*100),
       publicKey: import.meta.env.VITE_PAYSTACK_API,
     };
   
