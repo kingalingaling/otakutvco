6ff961fd62fc1969155a08e2c44217e6e98e297a
King <kingeshiebor@gmail.com>
2023-11-02 16:52:08 +0100

    Adjusted final cost for paystack fees

--- client/src/config/paystack.js
+++ client/src/config/paystack.js
@@ -3,7 +3,7 @@ const PaystackConfig = (email, cost) => {
       reference: new Date().getTime().toString(),
       email: email,
       //Currency in kobo
-      amount: (cost* 100),
+      amount: ((cost* 100) + 100 +(0.015*cost)),
       publicKey: import.meta.env.VITE_PAYSTACK_API,
     };
   
