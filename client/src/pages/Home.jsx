8dbfd84749153e360d5c1165ab160a7987893a87
King <kingeshiebor@gmail.com>
2023-11-11 12:10:34 +0100

    Added meta tags

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -9,10 +9,37 @@ import Feedback from "../components/Feedback";
 import Footer from "../components/Footer";
 import CountUp from "../components/CountUp";
 import GetTickets from "../components/GetTickets";
+import { Helmet } from "react-helmet";
 
 const Home = () => {
   return (
     <div className="w-full">
+      <Helmet>
+        <title>OtakuTv - Everything Anime</title>
+        <meta
+          name="description"
+          content="Join our community of over 40,000 anime enthusiasts to connect with fellow weebs and share your favorite moments"
+        />
+        <meta
+          name="keywords"
+          content="anime, manga, community, otaku connect, discussion, otakutv, otaku"
+        />
+        <meta property="og:title" content="OtakuTv - Everything Anime" />
+        <meta
+          property="og:description"
+          content="Join our community of over 40,000 anime enthusiasts to connect with fellow weebs and share your favorite moments"
+        />
+        <meta property="og:image" content="/assets/images/og/otakutv-og.jpg" />
+        <meta property="og:url" content="https://otakutv.co" />
+        {/* Twitter */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="OtakuTv - Everything Anime" />
+        <meta
+          name="twitter:description"
+          content="Join our community of over 40,000 anime enthusiasts to connect with fellow weebs and share your favorite moments"
+        />
+        <meta name="twitter:image" content="/assets/images/og/otakutv-og.jpg" />
+      </Helmet>
       <Navbar />
       <Hero />
       <GetTickets />
@@ -23,11 +50,15 @@ const Home = () => {
           <p className="font-bold text-xl text-center">Serivces</p>
         </div>
         <div className="flex flex-col items-center w-1/3">
-          <div className="flex font-bold text-red-600 text-xl md:text-2xl lg:text-4xl"><CountUp targetNumber={35000} step={100} interval={4} />+</div>
+          <div className="flex font-bold text-red-600 text-xl md:text-2xl lg:text-4xl">
+            <CountUp targetNumber={35000} step={100} interval={4} />+
+          </div>
           <p className="font-bold text-xl text-center">Members</p>
         </div>
         <div className="flex flex-col items-center w-1/3">
-          <div className="flex font-bold text-red-600 text-xl md:text-2xl lg:text-4xl"><CountUp targetNumber={13000} step={50} interval={4}/>+</div>
+          <div className="flex font-bold text-red-600 text-xl md:text-2xl lg:text-4xl">
+            <CountUp targetNumber={13000} step={50} interval={4} />+
+          </div>
           <p className="font-bold text-xl text-center">Anime</p>
         </div>
       </div>
--- client/src/pages/OtakuConnect.jsx
+++ client/src/pages/OtakuConnect.jsx
@@ -1,32 +1,62 @@
-import Footer from "../components/Footer"
-import Navbar from "../components/Navbar"
-import Expectations from "../components/OtakuConnect/Expectations"
-import Hero from '../components/OtakuConnect/Hero'
-import OtakuConnect2023 from "../components/OtakuConnect/OtakuConnect2023"
-import Partners from "../components/OtakuConnect/Partners"
-import Review from "../components/OtakuConnect/Review"
-import Summary from "../components/OtakuConnect/Summary"
-import { useNavigate } from "react-router-dom"
-import { useEffect } from "react"
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import Expectations from "../components/OtakuConnect/Expectations";
+import Hero from "../components/OtakuConnect/Hero";
+import OtakuConnect2023 from "../components/OtakuConnect/OtakuConnect2023";
+import Partners from "../components/OtakuConnect/Partners";
+import Review from "../components/OtakuConnect/Review";
+import Summary from "../components/OtakuConnect/Summary";
+import { useNavigate } from "react-router-dom";
+import { Helmet } from "react-helmet";
 
 const OtakuConnect = () => {
-  useEffect(() => {
-    document.title = "Otaku Connect '23"
-  }, [])
 
   const navigate = useNavigate();
   return (
-    <div className='w-full'>
-        <Navbar />
-        <Hero navigate={navigate}/>
-        <Summary />
-        <Review />
-        <OtakuConnect2023 navigate={navigate}/>
-        <Expectations />
-        <Partners />
-        <Footer />
+    <div className="w-full">
+      <Helmet>
+        <title>Otaku Connect &apos;23</title>
+        <meta
+          name="description"
+          content="Otaku Connect returns to Abuja and Lagos Nigeria for the 2023 edition!
+          An annual Anime Fest organized by OtakuTv for the weebs of Nigeria
+          Cosplay, Ramen, Anime Trivia, Charades, Movies and more!!! 
+          "
+        />
+        <meta
+          name="keywords"
+          content="anime, otaku connect, otakutv, otaku, manga, weeb, fest, otaku fest, community"
+        />
+        <meta property="og:title" content="Otaku Connect '23" />
+        <meta
+          property="og:description"
+          content="Otaku Connect returns to Abuja and Lagos Nigeria for the 2023 edition!
+          An annual Anime Fest organized by OtakuTv for the weebs of Nigeria
+          Cosplay, Ramen, Anime Trivia, Charades, Movies and more!!!"
+        />
+        <meta property="og:image" content="/assets/images/og/otakutv-og.jpg" />
+        <meta property="og:url" content="https://otakutv.co/otakuconnect" />
+        {/* Twitter */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="OtakuTv - Everything Anime" />
+        <meta
+          name="twitter:description"
+          content="Otaku Connect returns to Abuja and Lagos Nigeria for the 2023 edition!
+          An annual Anime Fest organized by OtakuTv for the weebs of Nigeria
+          Cosplay, Ramen, Anime Trivia, Charades, Movies and more!!!"
+        />
+        <meta name="twitter:image" content="/assets/images/og/OC-og.jpg" />
+      </Helmet>
+      <Navbar />
+      <Hero navigate={navigate} />
+      <Summary />
+      <Review />
+      <OtakuConnect2023 navigate={navigate} />
+      <Expectations />
+      <Partners />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default OtakuConnect
+export default OtakuConnect;
